9c5e1dcf90abf833d02a8cdbced9976510cd9b0e
yaoguo0083 <syhylgf@gmail.com>
2026-07-12 20:38:34 +0800

    Update _worker.js

--- CF-Worker/_worker.js
+++ CF-Worker/_worker.js
@@ -6,7 +6,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let 我的VL密钥 = '04c808e2-0b59-47b0-a54b-32fc7ef1c902';//UUID
+let 我的VL密钥 = '824ad220-8374-4e24-8107-a18fdedb0470';//UUID
 let 反代IP = 'proxyip.cmliussss.net'; //反代IP
 
 export default {
